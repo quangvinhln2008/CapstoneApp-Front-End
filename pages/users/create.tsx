1816c4981ed7a79fd71424805c0b44714fdabe6b
Nguyen Vinh <vinhnq.ufm@gmail.com>
2022-04-14 23:21:41 +0700

    feat: complete UI - User

---
+++ pages/users/create.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
+import React from 'react'
+import { Button, Form, FormGroup, Input, Label, Table } from 'reactstrap'
+import style from './User.module.css'
+
+function User() {
+  const router = useRouter()
+
+  const handleCancelClick = e => {
+    e.preventDefault()
+    router.push('/users')
+  }
+  return (
+    <>
+      <Head>
+        <title>Capstone Application | User</title>
+      </Head>
+      <div className="content__header">
+        <h2>Add new User</h2>
+      </div>
+      <Form className="content__form">
+        <FormGroup>
+          <Label for="userName">User name:</Label>
+          <Input id="userName" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email:</Label>
+          <Input id="email" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="role">Role:</Label>
+          <Input id="password" name="password" type="select">
+            <option value="owner">Owner</option>
+            <option value="project">Project Manager</option>
+            <option value="billing">Billing Manager</option>
+            <option value="developer">Developer</option>
+          </Input>
+        </FormGroup>
+        <div className="content__form_button">
+          <Button onClick={handleCancelClick} className="content__button_cancel" outline>
+            Cancel
+          </Button>
+          <Button className="content__button_save" color="success">
+            Add new user
+          </Button>
+        </div>
+      </Form>
+    </>
+  )
+}
+export default User
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,8 +1,18 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
 import React from 'react'
 import { Button, Input, Table } from 'reactstrap'
+import style from './User.module.css'
 
 function User() {
+  const router = useRouter()
+
+  const handleAddClick = e => {
+    e.preventDefault()
+    router.push('/users/create')
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +20,7 @@ function User() {
       </Head>
       <div className="content__header">
         <h2>USER LIST</h2>
-        <Button color="primary" className="button--add">
+        <Button color="primary" className="button--add" onClick={handleAddClick}>
           Add new Users
         </Button>
       </div>
@@ -23,29 +33,60 @@ function User() {
           <thead>
             <tr>
               <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>User Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
               <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>mark@gmail.com</td>
+              <td>
+                <span className={`${style.user__role} ${style.owner}`}>Owner</span>
+              </td>
+              <td>
+                <Link href="/users/1">
+                  <a className="link_edit">Edit</a>
+                </Link>
+                <Link href="/users/1">
+                  <a className="link_delete">Delete</a>
+                </Link>
+              </td>
             </tr>
             <tr>
               <th scope="row">2</th>
               <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <td>Jacob@gmail.com</td>
+              <td>
+                <span className={`${style.user__role} ${style.project}`}>Project</span>
+              </td>
+              <td>
+                <Link href="/users/1">
+                  <a className="link_edit">Edit</a>
+                </Link>
+                <Link href="/users/1">
+                  <a className="link_delete">Delete</a>
+                </Link>
+              </td>
             </tr>
             <tr>
               <th scope="row">3</th>
               <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
+              <td>larry@gmail.com</td>
+              <td>
+                <span className={`${style.user__role} ${style.developer}`}>Developer</span>
+              </td>
+              <td>
+                <Link href="/users/1">
+                  <a className="link_edit">Edit</a>
+                </Link>
+                <Link href="/users/1">
+                  <a className="link_delete">Delete</a>
+                </Link>
+              </td>
             </tr>
           </tbody>
         </Table>
