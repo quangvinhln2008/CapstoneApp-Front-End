804aefc97ccf914239ba5e192a0c67da4a24297a
Nguyen Vinh <vinhnq.ufm@gmail.com>
2022-04-18 22:23:14 +0700

    SCP-31| Add UI project
    Add form project list , add new project, edit project

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 import Router, { useRouter } from 'next/router'
 import React, { MouseEvent, useState } from 'react'
 import { Button, Input, Modal, ModalBody, ModalFooter, ModalHeader, Table } from 'reactstrap'
-import style from './user.module.scss'
+import style from './project.module.scss'
 
-const User = () => {
+const Project = () => {
   const router = useRouter()
   const [modalForm, setModalForm] = useState(false)
 
   function handleAddClick(e: MouseEvent): void {
     e.preventDefault()
-    router.push('/users/create')
+    router.push('/projects/create')
   }
 
   function handleToogleModalForm(): void {
@@ -21,39 +21,41 @@ const User = () => {
   return (
     <>
       <Head>
-        <title>Capstone Application | User</title>
+        <title>Capstone Application | Projects</title>
       </Head>
       <div className="contentHeader">
-        <h2>User List</h2>
+        <h2>Projects List</h2>
         <Button color="primary" className="buttonAdd" onClick={handleAddClick}>
-          Add new Users
+          Add New Project
         </Button>
       </div>
       <div className="contentFilter">
         <span>Filter by:</span>
-        <Input id="userName" name="userName" placeholder="User name....." type="text" />
+        <Input id="projectName" name="projectName" placeholder="Project name....." type="text" />
       </div>
       <div className="contentTable">
         <Table hover>
           <thead>
             <tr>
               <th>#</th>
-              <th>User Name</th>
-              <th>Email</th>
-              <th>Role</th>
+              <th>Project Name</th>
+              <th>Decription</th>
+              <th>Create at</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
-              <td>Mark</td>
-              <td>mark@gmail.com</td>
               <td>
-                <span className={`${style.userRole} ${style.owner}`}>Owner</span>
+                <Link href="/projects/1/edit">
+                  <a className={style.projectDetail}>Capstone project</a>
+                </Link>
               </td>
+              <td>Lorem Ipsum has been</td>
+              <td>2018-09-27</td>
               <td>
-                <Link href="/users/1/edit">
+                <Link href="/projects/1/edit">
                   <a className="linkEdit">Edit</a>
                 </Link>
                 <button className={`${style.buttonLink} ${style.delete}`} onClick={handleToogleModalForm}>
@@ -63,13 +65,15 @@ const User = () => {
             </tr>
             <tr>
               <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Jacob@gmail.com</td>
               <td>
-                <span className={`${style.userRole} ${style.project}`}>Project</span>
+                <Link href="/projects/1/edit">
+                  <a className={style.projectDetail}>Capstone project</a>
+                </Link>
               </td>
+              <td>Lorem Ipsum has been</td>
+              <td>2018-09-27</td>
               <td>
-                <Link href="/users/1/edit">
+                <Link href="/projects/1/edit">
                   <a className="linkEdit">Edit</a>
                 </Link>
                 <button className={`${style.buttonLink} ${style.delete}`} onClick={handleToogleModalForm}>
@@ -79,13 +83,15 @@ const User = () => {
             </tr>
             <tr>
               <th scope="row">3</th>
-              <td>Larry</td>
-              <td>larry@gmail.com</td>
               <td>
-                <span className={`${style.userRole} ${style.developer}`}>Developer</span>
+                <Link href="/projects/1/edit">
+                  <a className={style.projectDetail}>Capstone project</a>
+                </Link>
               </td>
+              <td>Lorem Ipsum has been</td>
+              <td>2018-09-27</td>
               <td>
-                <Link href="/users/1/edit">
+                <Link href="/projects/1/edit">
                   <a className="linkEdit">Edit</a>
                 </Link>
                 <button className={`${style.buttonLink} ${style.delete}`} onClick={handleToogleModalForm}>
@@ -97,8 +103,8 @@ const User = () => {
         </Table>
       </div>
       <Modal isOpen={modalForm} toggle={handleToogleModalForm} backdrop="static">
-        <ModalHeader toggle={handleToogleModalForm}>Delete user</ModalHeader>
-        <ModalBody>Do you want delete user...?</ModalBody>
+        <ModalHeader toggle={handleToogleModalForm}>Delete project</ModalHeader>
+        <ModalBody>Do you want delete project...?</ModalBody>
         <ModalFooter>
           <Button color="danger">Delete</Button>{' '}
           <Button outline color="secondary" onClick={handleToogleModalForm}>
@@ -109,4 +115,4 @@ const User = () => {
     </>
   )
 }
-export default User
+export default Project
